056ef3089b822dd80589d96edaf59071b4b62cfc
Cerys <cerysamberlewis@gmail.com>
2025-09-20 15:04:26 +0100

    Update Program.cs

--- AuxiliumMicroservices/Program.cs
+++ AuxiliumMicroservices/Program.cs
@@ -17,6 +17,7 @@ namespace AuxiliumMicroservices
   / ____ \ |_| |>  <| | | | |_| | | | | | |        | | (_| \__ \   <      | | \ \ |_| | | | | | | |  __/ |   
  /_/    \_\__,_/_/\_\_|_|_|\__,_|_| |_| |_|        |_|\__,_|___/_|\_\     |_|  \_\__,_|_| |_|_| |_|\___|_|   
 ");
+
             ArgumentParsing.SortConfigFileLocation(args);
             await Preflight.Go();
         }
