6c6a725ba9979b1abaebbdf7e9b9b49fba2df342
Cerys <cerysamberlewis@gmail.com>
2025-11-17 12:48:56 +0000

    Update StateTableWriter.cs

--- AuxiliumMicroservices/Common/Utilities/StateTableWriter.cs
+++ AuxiliumMicroservices/Common/Utilities/StateTableWriter.cs
@@ -13,58 +13,62 @@ namespace AuxiliumMicroservices.Common.Utilities
 
         internal static async Task<bool> OutputStatusTable(CancellationToken cancellationToken)
         {
-            const int queueNameWidth = 30;
-            const int statusWidth = 20;
-
-            string separator = new('-', queueNameWidth + (statusWidth * 3) + (3 * 3) + 4);
-
-            if (!firstRender)
+            while(!cancellationToken.IsCancellationRequested)
             {
-                Console.SetCursorPosition(0, Console.CursorTop - lastTotalLines);
-            }
+                const int queueNameWidth = 30;
+                const int statusWidth = 20;
 
-            Console.WriteLine(separator);
-            Console.WriteLine($"| {"Queue",-queueNameWidth} | {"Successful Jobs",-statusWidth} | {"Failed Jobs",-statusWidth} | {"Total Jobs",-statusWidth} |");
-            Console.WriteLine(separator);
+                string separator = new('-', queueNameWidth + (statusWidth * 3) + (3 * 3) + 4);
 
-            int currentRowCount = 0;
-            foreach (var (QueueName, consumerDetails) in ConsumerController.GetConsumers())
-            {
-                Console.Write($"| {QueueName,-queueNameWidth} | ");
-
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write($"{consumerDetails.GetSuccessfulJobs(),-statusWidth}");
-                Console.ResetColor();
-                Console.Write(" | ");
-
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write($"{consumerDetails.GetFailedJobs(),-statusWidth}");
-                Console.ResetColor();
-                Console.Write(" | ");
-
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.Write($"{consumerDetails.GetTotalJobs(),-statusWidth}");
-                Console.ResetColor();
-                Console.WriteLine(" |");
-
-                currentRowCount++;
-            }
-
-            Console.WriteLine(separator);
-
-            int currentTotalLines = currentRowCount + 4;
-
-            if (currentTotalLines < lastTotalLines)
-            {
-                for (int i = 0; i < (lastTotalLines - currentTotalLines); i++)
+                if (!firstRender)
                 {
-                    Console.WriteLine(new string(' ', separator.Length));
+                    Console.SetCursorPosition(0, Console.CursorTop - lastTotalLines);
                 }
+
+                Console.WriteLine(separator);
+                Console.WriteLine($"| {"Queue",-queueNameWidth} | {"Successful Jobs",-statusWidth} | {"Failed Jobs",-statusWidth} | {"Total Jobs",-statusWidth} |");
+                Console.WriteLine(separator);
+
+                int currentRowCount = 0;
+                foreach (var (QueueName, consumerDetails) in ConsumerController.GetConsumers())
+                {
+                    Console.Write($"| {QueueName,-queueNameWidth} | ");
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write($"{consumerDetails.GetSuccessfulJobs(),-statusWidth}");
+                    Console.ResetColor();
+                    Console.Write(" | ");
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write($"{consumerDetails.GetFailedJobs(),-statusWidth}");
+                    Console.ResetColor();
+                    Console.Write(" | ");
+
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.Write($"{consumerDetails.GetTotalJobs(),-statusWidth}");
+                    Console.ResetColor();
+                    Console.WriteLine(" |");
+
+                    currentRowCount++;
+                }
+
+                Console.WriteLine(separator);
+
+                int currentTotalLines = currentRowCount + 4;
+
+                if (currentTotalLines < lastTotalLines)
+                {
+                    for (int i = 0; i < (lastTotalLines - currentTotalLines); i++)
+                    {
+                        Console.WriteLine(new string(' ', separator.Length));
+                    }
+                }
+
+                lastTotalLines = currentTotalLines;
+                firstRender = false;
+
+
             }
-
-            lastTotalLines = currentTotalLines;
-            firstRender = false;
-
             return true;
         }
     }
