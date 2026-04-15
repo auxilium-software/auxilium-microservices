932796f9b2b817e5399f0b12b778de5ac53aa99c
Cerys <cerysamberlewis@gmail.com>
2026-04-15 02:05:51 +0100

    interfaces

---
+++ AuxiliumMicroservices/Services/IEmailTemplateRenderer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuxiliumSoftware.AuxiliumServices.BackgroundTaskRunner.Services
+{
+    public interface IEmailTemplateRenderer
+    {
+        string Render(string templateName, Dictionary<string, string> data);
+    }
+}
